7ae0f3263609b4bca08207b826ebc8b6b3583141
Yadir <yamunozr22@itp.edu.co>
2025-05-20 19:12:18 -0500

    [US-1.1] feat: Configuración inicial de NestJS #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './infrastructure/exceptions/global-exception.filter';
+import { LoggerService } from './infrastructure/logging/logger.service';
+import appConfig from './infrastructure/config/app.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+  ],
+  controllers: [],
+  providers: [
+    LoggerService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
